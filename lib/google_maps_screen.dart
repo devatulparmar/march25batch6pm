96e768f579d6dcb2b381e859ce08dc0210ff0b31
atul <dev.atulparmar@gmail.com>
2025-08-14 18:08:52 +0530

    add Markers, Polyline, Add Polygon,  Add Circle to Google Maps

--- lib/google_maps_screen.dart
+++ lib/google_maps_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
@@ -12,8 +14,12 @@ class GoogleMapsScreen extends StatefulWidget {
 
 class _GoogleMapsScreenState extends State<GoogleMapsScreen> {
   bool isLoading = false;
-  late LatLng latLng;
+  late LatLng currantLatLng;
   late GoogleMapController _googleMapController;
+  final Set<Circle> _circles = {};
+  final Set<Polygon> _polygons = {};
+  final Set<Marker> _markers = {};
+  final Set<Polyline> _polyline = {};
 
   CameraPosition defaultCameraPosition = const CameraPosition(
     target: LatLng(0, 0),
@@ -23,16 +29,89 @@ class _GoogleMapsScreenState extends State<GoogleMapsScreen> {
   Future _fetchCurrantLocation() async {
     LocationData data = await FindMyLocation.getCurrantLocation();
     setState(() {
-      latLng = LatLng(
+      currantLatLng = LatLng(
         data.latitude!.toDouble(),
         data.longitude!.toDouble(),
       );
     });
+    await _addCircleToMap();
+    _setPolygon();
+    _addMarkers();
+    _addPolyline();
+  }
+
+  Future _addCircleToMap() async {
+    _circles.add(
+      Circle(
+        circleId: const CircleId('myCircle'),
+        center: currantLatLng,
+        radius: 1000,
+        // 1000 meters radius
+        fillColor: Colors.red.withOpacity(0.3),
+        strokeColor: Colors.red,
+        strokeWidth: 2,
+      ),
+    );
+  }
+
+  void _setPolygon() {
+    _polygons.add(
+      Polygon(
+        polygonId: const PolygonId('polygon_1'),
+        points: const [
+          LatLng(37.43296265331129, -122.08832357078792),
+          LatLng(37.42796133580664, -122.085749655962),
+          LatLng(37.418, -122.09),
+          LatLng(37.423, -122.094),
+        ],
+        strokeColor: Colors.blue,
+        strokeWidth: 3,
+        fillColor: Colors.blue.withOpacity(0.2),
+      ),
+    );
+    setState(() {});
+  }
+
+  void _addMarkers() {
+    _markers.add(
+      const Marker(
+        markerId: MarkerId('marker_1'),
+        position: LatLng(37.42796133580664, -122.085749655962),
+        infoWindow: InfoWindow(title: 'Ahmedabad', snippet: 'This is marker 1'),
+      ),
+    );
+
+    _markers.add(
+      const Marker(
+        markerId: MarkerId('marker_2'),
+        position: LatLng(37.430, -122.090),
+        infoWindow: InfoWindow(title: 'Vadodara', snippet: 'This is marker 2'),
+      ),
+    );
+
+    setState(() {}); // Refresh map to show markers
+  }
+
+  void _addPolyline() {
+    _polyline.add(
+      const Polyline(
+        polylineId: PolylineId('polyline_1'),
+        points: [
+          LatLng(37.42796133580664, -122.085749655962),
+          LatLng(37.430, -122.090),
+        ],
+        color: Colors.pink,
+        width: 5,
+      ),
+    );
+
+    setState(() {}); // Refresh map to show polylines
   }
 
   @override
   void initState() {
     super.initState();
+    _fetchCurrantLocation();
   }
 
   @override
@@ -62,7 +141,7 @@ class _GoogleMapsScreenState extends State<GoogleMapsScreen> {
             _googleMapController.animateCamera(
               CameraUpdate.newCameraPosition(
                 CameraPosition(
-                  target: latLng,
+                  target: currantLatLng,
                   zoom: 15,
                 ),
               ),
@@ -83,7 +162,7 @@ class _GoogleMapsScreenState extends State<GoogleMapsScreen> {
         },
         buildingsEnabled: true,
         compassEnabled: true,
-        liteModeEnabled: true,
+        liteModeEnabled: false,
         mapToolbarEnabled: false,
         fortyFiveDegreeImageryEnabled: true,
         indoorViewEnabled: true,
@@ -92,6 +171,24 @@ class _GoogleMapsScreenState extends State<GoogleMapsScreen> {
         scrollGesturesEnabled: true,
         tiltGesturesEnabled: true,
         trafficEnabled: true,
+        myLocationEnabled: true,
+        zoomControlsEnabled: true,
+        zoomGesturesEnabled: true,
+        cameraTargetBounds: CameraTargetBounds.unbounded,
+        minMaxZoomPreference: MinMaxZoomPreference.unbounded,
+        layoutDirection: TextDirection.ltr,
+        circles: _circles,
+        gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
+          Factory<OneSequenceGestureRecognizer>(() => EagerGestureRecognizer()),
+        },
+        polygons: _polygons,
+        markers: _markers,
+        polylines: _polyline,
+        onTap: (value) {},
+        onLongPress: (value) {},
+        onCameraIdle: () {},
+        onCameraMoveStarted: () {},
+        onCameraMove: (CameraPosition position) {},
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:march25batch6pm/home_screen.dart';
-import 'package:march25batch6pm/services/notification_service.dart';
-import 'package:march25batch6pm/services/push_notification_service.dart';
 import 'package:march25batch6pm/utils/color_const.dart';
 import 'package:march25batch6pm/utils/const.dart';
 import 'package:march25batch6pm/utils/routes.dart';
